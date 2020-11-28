d971ff22664d9340b51bfbdce452b4c050aa636f
frdt98 <frdt98@gmail.com>
2020-11-29 01:02:00 +0900

    Finished basic UI

--- front-end/main.js
+++ front-end/main.js
@@ -19,7 +19,7 @@ function getScreenshot(videoEl, emotion, scale) {
     canvas.width = videoEl.clientWidth * scale;
     canvas.height = videoEl.clientHeight * scale;
     canvas.getContext('2d').drawImage(videoEl, 0, 0, canvas.width, canvas.height);
-    window.localStorage.setItem(emotion, canvas.toDataURL())
+    console.log(emotion)
     const image = new Image()
     image.src = canvas.toDataURL();
     return image;
@@ -50,13 +50,22 @@ const gameInit = () => {
 
 
     // Camera stream
-    const feed = el("video.mx-auto", {id: "videoElement", autoplay: "true"})
-    const cameraFeed = el(".row", feed)
+    const videoStream = el("video.mx-auto row", {id: "videoElement", autoplay: "true"})
+    const feed = el(".col-6", videoStream, 
+    el(".row",el("button.btn btn-primary mx-auto", { style: {color: "white"}, innerText: "Capture", type: "button", "data-toggle": "modal", "data-target": "#basicExampleModal",
+    onclick: () => {
+        let picture = getScreenshot(videoStream, gameEntries[current])
+        picture.classList = "w100"
+        console.log(gameEntries, current)
+        setChildren(document.getElementById("popupImage"), picture)
+    } })
+    ))
+    const cameraFeed = el(".row", el(".col-6", {id: "trigger"}), feed)
     setChildren(document.getElementById("root"), cameraFeed)    
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true })
           .then(function (stream) {
-            feed.srcObject = stream;
+            videoStream.srcObject = stream;
           })
           .catch(function (error) {
             console.log("Something went wrong!");
@@ -64,22 +73,20 @@ const gameInit = () => {
     }
 
     mount(main, cameraFeed)
-    mount(main, el("button.btn btn-primary", { innerText: "Capture", type: "button", "data-toggle": "modal", "data-target": "#basicExampleModal",
-    onclick: () => {
-        let picture = getScreenshot(feed, gameEntries[current])
-        picture.classList = "w100"
-        console.log(gameEntries, current)
-        setChildren(document.getElementById("popupImage"), picture)
-    } }))
     mount(main, el(".btn-toolbar mb-4", {role: "toolbar"}, score))
 
 
 
 
     setChildren(document.getElementById("root"), main)
-    
+    initImage()
 }
 
+
+const initImage = () => {
+    setChildren(document.getElementById("trigger"), el("", {style: {backgroundImage: `url("${db[gameEntries[current]]}")`, width: '100%', height: "100%", backgroundSize: "cover"}}))
+    current +=1
+}
 
 
 
